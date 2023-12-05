dc8539e812883c40b1559086118ccebc7ca77f18
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-12-05 14:48:26 +0300

    day14

---
+++ src/test/java/day14_testNg/C07_CheckBoxTest.java
@@ -0,0 +1,42 @@
+package day14_testNg;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.TestOtomasyonuFormPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C07_CheckBoxTest {
+    @Test
+    public void test01(){
+        //	a. Verilen web sayfasına gidin.
+        //	     https://testotomasyonu.com/form
+        Driver.getDriver().get("https://testotomasyonu.com/form");
+
+        //	b. Sirt Agrisi ve Carpinti checkbox’larini secin
+        Actions actions = new Actions(Driver.getDriver());
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+        TestOtomasyonuFormPage testOtomasyonuFormPage = new TestOtomasyonuFormPage();
+        testOtomasyonuFormPage.carpintiCheckboxKutusu.click();
+        testOtomasyonuFormPage.sirtAgrisiCheckboxKutusu.click();
+
+        //	c. Sirt Agrisi ve Carpinti checkbox’larininin seçili olduğunu test edin
+
+        Assert.assertTrue(testOtomasyonuFormPage.carpintiCheckboxKutusu.isSelected());
+        Assert.assertTrue(testOtomasyonuFormPage.sirtAgrisiCheckboxKutusu.isSelected());
+
+        //	d. Seker ve Epilepsi checkbox’larininin seçili olmadigini test edin
+
+        actions.sendKeys(Keys.PAGE_DOWN).perform();
+        ReusableMethods.bekle(1);
+
+        Assert.assertFalse(testOtomasyonuFormPage.epilepsiCheckboxKutusu.isSelected());
+        Assert.assertFalse(testOtomasyonuFormPage.sekerCheckboxKutusu.isSelected());
+        ReusableMethods.bekle(2);
+        Driver.closeDriver();
+    }
+}
