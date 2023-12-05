9209c6c38146b3ecf3feaeb245d737c304d91b05
UgurcanAydogan <ugurcanaydogan88@gmail.com>
2023-12-05 13:55:36 +0300

    day14

--- src/test/java/day14_testNg/C03_AramaTesti.java
+++ src/test/java/day14_testNg/C03_AramaTesti.java
@@ -2,8 +2,10 @@ package day14_testNg;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,32 +21,54 @@ public class C03_AramaTesti {
     //    arama sonucunda urun bulunabildigini test edin
     // 3- ilk urune tiklayip
     //    acilan urun sayfasinda, urun isminin case sensitive olmadan phone icerdigini test edin
-
     WebDriver driver;
 
-    @Test(priority = 1)
-    public void anaSayfaTesti(){
+    @Test
+    public void testOtomasyonuTesti(){
+
         WebDriverManager.chromedriver().setup();
+        driver= new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         // 1- testotomasyonu anasayfaya gidip
         //    dogru adrese gittigimizi test edin
         driver.get("https://www.testotomasyonu.com");
 
-        String expectedUrl = "https://www.testotomasyonu.com";
+        String expectedUrl ="https://www.testotomasyonu.com/";
         String actualUrl = driver.getCurrentUrl();
-        Assert.assertEquals(expectedUrl,actualUrl);
-
+        Assert.assertEquals(actualUrl,expectedUrl);
     }
-    @Test(priority = 2)
+
+    @Test(dependsOnMethods = "testOtomasyonuTesti")
     public void aramaTesti(){
         // 2- phone icin arama yapip
         //    arama sonucunda urun bulunabildigini test edin
         WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone" + Keys.ENTER);
+
+        WebElement aramaSonucElementi = driver.findElement(By.className("product-count-text"));
+
+        String unexpectedSonucYazisi = "0 Products Found";
+        String actualSonucyazisi = aramaSonucElementi.getText();
+
+        Assert.assertNotEquals(unexpectedSonucYazisi,actualSonucyazisi);
     }
-    @Test(priority = 3)
+
+    @Test(dependsOnMethods = "aramaTesti")
     public void urunIsimTesti(){
         // 3- ilk urune tiklayip
         //    acilan urun sayfasinda, urun isminin case sensitive olmadan phone icerdigini test edin
+
+        driver.findElement(By.xpath("(//*[@class='product-box my-2  py-1'])[1]"))
+                .click();
+
+        WebElement urunIsimElementi = driver.findElement(By.xpath(" //div[@class=' heading-sm mb-4']"));
+
+        String expectedIsimIcerik = "phone";
+        String actualIsimKucukHarf = urunIsimElementi.getText().toLowerCase();
+
+        Assert.assertTrue(actualIsimKucukHarf.contains(expectedIsimIcerik));
+
+        driver.close();
     }
 }
